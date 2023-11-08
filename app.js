6d7ef98f8c877f8822dd6a10e9432e1fb6b0d98a
Ramualdo Arias <ramiroarias2004@gmail.com>
2023-11-08 10:06:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function invertirCadena(cadena) {
+    let cadenaInvertida = '';
+  
+    for (let i = cadena.length - 1; i >= 0; i--) {
+      cadenaInvertida += cadena[i];
+    }
+  
+    return cadenaInvertida;
+  }
+  
+  // Ejemplo de uso:
+  const texto = "Hola, mundo!";
+  const textoInvertido = invertirCadena(texto);
+  console.log(textoInvertido);
+  
